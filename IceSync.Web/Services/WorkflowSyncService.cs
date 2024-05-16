bd4a78c700e435a9f238b69ab0f18abe2b9fde04
Denis Georgiev <d.georgiev.91@gmail.com>
2024-05-16 03:20:25 +0300

    Fix bulk operations

--- IceSync.Web/Services/WorkflowSyncService.cs
+++ IceSync.Web/Services/WorkflowSyncService.cs
@@ -1,6 +1,8 @@
-﻿using EFCore.BulkExtensions;
+﻿using AutoMapper;
+using EFCore.BulkExtensions;
 using IceSync.ApiClient;
 using IceSync.Data;
+using IceSync.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace IceSync.Web.Services;
@@ -10,11 +12,13 @@ public class WorkflowSyncService
     private readonly IServiceProvider _serviceProvider;
     private readonly IApiClient _apiClient;
     private readonly ILogger<WorkflowSyncService> _logger;
+    private readonly IMapper _mapper;
 
-    public WorkflowSyncService(IServiceProvider serviceProvider, IApiClient apiClient, ILogger<WorkflowSyncService> logger)
+    public WorkflowSyncService(IServiceProvider serviceProvider, IApiClient apiClient, IMapper mapper, ILogger<WorkflowSyncService> logger)
     {
         _serviceProvider = serviceProvider;
         _apiClient = apiClient;
+        _mapper = mapper;
         _logger = logger;
     }
 
@@ -32,21 +36,60 @@ public class WorkflowSyncService
 
             var workflowsToAdd = workflowsFromApi.Where(apiWorkflow => workflowsFromDb.All(dbWorkflow => dbWorkflow.Id != apiWorkflow.Id)).ToList();
             var workflowsToDelete = workflowsFromDb.Where(dbWorkflow => workflowsFromApi.All(apiWorkflow => apiWorkflow.Id != dbWorkflow.Id)).ToList();
-            var workflowsToUpdate = workflowsFromDb.Where(dbWorkflow => workflowsFromApi.Any(apiWorkflow => apiWorkflow.Id == dbWorkflow.Id)).ToList();
+
+            var workflowsToUpdate = new List<Workflow>();
+
+            foreach (var dbWorkflow in workflowsFromDb)
+            {
+                var apiWorkflow = workflowsFromApi.FirstOrDefault(w => w.Id == dbWorkflow.Id);
+                if (apiWorkflow != null)
+                {
+                    var isUpdated = false;
+
+                    if (dbWorkflow.Name != apiWorkflow.Name)
+                    {
+                        dbWorkflow.Name = apiWorkflow.Name;
+                        isUpdated = true;
+                    }
+
+                    if (dbWorkflow.IsActive != apiWorkflow.IsActive)
+                    {
+                        dbWorkflow.IsActive = apiWorkflow.IsActive;
+                        isUpdated = true;
+                    }
+
+                    if (dbWorkflow.IsRunning != apiWorkflow.IsRunning)
+                    {
+                        dbWorkflow.IsRunning = apiWorkflow.IsRunning;
+                        isUpdated = true;
+                    }
+
+                    if (dbWorkflow.MultiExecBehavior != apiWorkflow.MultiExecBehavior)
+                    {
+                        dbWorkflow.MultiExecBehavior = apiWorkflow.MultiExecBehavior;
+                        isUpdated = true;
+                    }
+
+                    if (isUpdated)
+                    {
+                        workflowsToUpdate.Add(dbWorkflow);
+                    }
+                }
+            }
 
             if (workflowsToAdd.Any())
             {
-                await dbContext.BulkInsertAsync(workflowsToAdd);
+                await dbContext.BulkInsertAsync(_mapper.Map<IEnumerable<Workflow>>(workflowsToAdd));
             }
 
             if (workflowsToDelete.Any())
             {
-                await dbContext.BulkDeleteAsync(workflowsToDelete);
+                await dbContext.BulkDeleteAsync(_mapper.Map<IEnumerable<Workflow>>(workflowsToDelete));
             }
 
             if (workflowsToUpdate.Any())
             {
-                await dbContext.BulkUpdateAsync(workflowsToUpdate);
+                await dbContext.BulkUpdateAsync(_mapper.Map<IEnumerable<Workflow>>(workflowsToUpdate));
             }
 
             _logger.LogInformation("Workflow synchronization completed successfully.");
